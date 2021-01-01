474d3ac8696db0357f5d9e7ec0a8e7155fdf3581
TAOSAND <taofeekdboy@yahoo.com>
2021-01-01 10:57:28 +0400

    larVue

--- resources/js/router.js
+++ resources/js/router.js
@@ -1,5 +1,5 @@
 import Vue from 'vue';
-import VueRouter from 'vue-Router';
+import VueRouter from 'vue-router';
 import Login from './components/Login';
 import ExampleComponent from './components/ExampleComponent'
 
